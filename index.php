3f624c25b7f219c87b7f8900e92b428dfeaac288
Courieux <courieuxa@gmail.com>
2018-11-14 23:38:00 +0100

    index.php and constants file added.

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: couri
+ * Date: 14/11/2018
+ * Time: 23:03
+ */
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title><?php echo WEBSITE_NAME ?></title>
+</head>
+<body>
+    <p>Hello World !</p>
+</body>
+</html>
---
+++ php/constants.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: whitedumb
+ * Date: 14/11/2018
+ * Time: 23:18
+ */
+define("WEBSITE_NAME","SillyTV");
